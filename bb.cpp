63c4f7f70a83874d26f3550ef09899353a81797a
josedavid <jdfr@users.noreply.github.com>
2015-11-27 12:58:15 +0100

    add dimensions to the tool to compute the bounding box of a mesh

--- bb.cpp
+++ bb.cpp
@@ -21,9 +21,13 @@ int main(int argc, char **argv) {
   Slic3r::BoundingBoxf3 bb = mesh->bounding_box();
   
   printf("bounding box:\n");
-  printf("X: %25.20g %25.20g\n", bb.min.x, bb.max.x);
-  printf("Y: %25.20g %25.20g\n", bb.min.y, bb.max.y);
-  printf("Z: %25.20g %25.20g\n", bb.min.z, bb.max.z);
+  printf(" X: %25.20g %25.20g\n", bb.min.x, bb.max.x);
+  printf(" Y: %25.20g %25.20g\n", bb.min.y, bb.max.y);
+  printf(" Z: %25.20g %25.20g\n", bb.min.z, bb.max.z);
+  printf("dimensions:\n");
+  printf(" dX: %25.20g\n", bb.max.x - bb.min.x);
+  printf(" dY: %25.20g\n", bb.max.y - bb.min.y);
+  printf(" dZ: %25.20g\n", bb.max.z - bb.min.z);
   
   delete mesh; 
   return 0;
