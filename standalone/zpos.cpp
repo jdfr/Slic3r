4f63c9c0d3f72d2e6ccf4a5c63548cfee61c12ef
josedavid <jdfr@users.noreply.github.com>
2016-02-29 15:13:42 +0100

    make zpos utility more versatile

--- standalone/zpos.cpp
+++ standalone/zpos.cpp
@@ -1,13 +1,16 @@
 #include "TriangleMesh.hpp"
 #include <stdio.h>
+#include <ctype.h>
 
 int main(int argc, char **argv) {
 
   if (argc<3) {
-    fprintf(stderr, "%d IS AN INCORRECT NUMBER OF ARGUMENTS!!!", argc-1);
+    fprintf(stderr, "USAGE: %s INPUT.STL OUTPUT.STL [NEGATIVE]\n  This utility reads a 3D mesh from a STL file, centers it in the XY plane and displaces it so the lowest coordinate is at 0. If an additional argument starting with the letter 'n' ('negative') is given, the Z displacement is so the highest coordinate is 0.\n", argv[0]);
     return -1;
   }
   char *filename=argv[1];
+  
+  bool negative = (argc>=4) && (tolower(argv[3][0])=='n');
   
   Slic3r::TriangleMesh *mesh = new Slic3r::TriangleMesh();
   mesh->stl.err = stderr;
@@ -20,7 +23,10 @@ int main(int argc, char **argv) {
 
   Slic3r::BoundingBoxf3 bb = mesh->bounding_box();
   
-  mesh->translate(0, 0, -bb.min.z);
+  double centerX = (bb.min.x + bb.max.x)/2;
+  double centerY = (bb.min.y + bb.max.y)/2;
+  
+  mesh->translate(-centerX, -centerY, negative ? -bb.max.z : -bb.min.z);
   
   mesh->write_binary(argv[2]);
   
