9e26d1f726808fdcdcffc06fa20b5c75c00461c6
Jose David <josedavid@uma.es>
2017-03-10 12:36:33 +0100

    reorganize cmake config values

---
+++ config.cmake
@@ -0,0 +1,7 @@
+#configuration
+option(CLIPPER_USE_ARENA       "enable ArenaMemoryManager for ClipperLib" ON)
+set(CLIPPER_BASE_DIR           "" CACHE PATH "tweaked clipper/iopaths base dir")
+set(INITIAL_ARENA_SIZE  "52428800" CACHE STRING "Initial arena size for ArenaMemoryManager")
+set(BIGCHUNK_ARENA_SIZE "2097152"  CACHE STRING "Bigchunk size for ArenaMemoryManager") #1-5 MB: 1048576 2097152 3145728 4194304 5242880
+set(BOOST_ROOT_PATH            "" CACHE PATH "path prefix to boost library")
+option(ENABLE_SLICER_LOGGING   "enable slicer debug output to file" ON)
